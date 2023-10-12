e59016221f4907cdee443e882e8e4a1f35b21910
Nikita Goswami <113491032+Nikieta@users.noreply.github.com>
2023-10-12 23:54:16 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";
+// alert(3+3). we are using nodejs,not browser
+
+console.log(3+3) ;
+console.log("s")
+console.log(3+3) //code readablity must be high
+let name="hitesh"
+let age=18;
+let isLoggedIn=false;
+let state;
+
+
+// number=> 2 to the power of 53
+// bigint
+// string=>""
+// boolean=>true/false
+// null =>standalone value; it is a special value 
+// let state=null;
+// undefined=>value has not been defined
+// used in react symbol=> used for uniqueness
+
+
+//object
+console.log(typeof "hitesh") //string
+console.log(typeof null) //object 
+console.log(typeof age) //number
+console.log(typeof undefined) //undefined
+
