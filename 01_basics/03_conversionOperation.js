6e76771a6ed70867da4411041a3152f835ac10a6
Nikita Goswami <113491032+Nikieta@users.noreply.github.com>
2023-10-15 02:04:35 +0000

    Conversion-Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,61 +1,94 @@
-// let score="33abc";
-// console.log(typeof score);//
-// console.log(typeof(score));
+let score="33abc";
+console.log(typeof score);//
+console.log(typeof(score));
 
-// let valueInNumber=Number(score);
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
-// let score=null;
-// console.log(typeof score);//
-// console.log(typeof(score));
+let score=null;
+console.log(typeof score);//
+console.log(typeof(score));
 
-// let valueInNumber=Number(score);
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
-// let score="null";
-// console.log(typeof score);//
-// console.log(typeof(score));
+let score="null";
+console.log(typeof score);//
+console.log(typeof(score));
 
-// let valueInNumber=Number(score);
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
-// let score=undefined;
-// console.log(typeof score);//
-// console.log(typeof(score));
+let score=undefined;
+console.log(typeof score);//
+console.log(typeof(score));
 
-// let valueInNumber=Number(score);
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
-// let score=true;
-// console.log(typeof score);//
-// console.log(typeof(score));
+let score=true;
+console.log(typeof score);//
+console.log(typeof(score));
 
-// let valueInNumber=Number(score);
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
-// "33"=>33
-// "33abc=>NaN"
-// true=>1;false=>0
+"33"=>33
+"33abc=>NaN"
+true=>1;false=>0
 
-// let isLoggedIn=" ";
-// let booleanIsLoggedin=Boolean(isLoggedIn)
-// console.log(booleanIsLoggedin);
+let isLoggedIn=" ";
+let booleanIsLoggedin=Boolean(isLoggedIn)
+console.log(booleanIsLoggedin);
 
-// ""=>false
-// " "=>true
-// 0 => false
-// 1 => true;
-// "anything inside this string be it a blank space"=>true;
+""=>false
+" "=>true
+0 => false
+1 => true;
+"anything inside this string be it a blank space"=>true;
 
 let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+let value=3
+let negValue= -value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+
+let str1="hello"
+let str2=" hitesh"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log(+true)
+console.log(+"")
+
+let num1,num2,num3;
+num1=num2=num3=2+2;
+ 
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter)
+
+
+
 
 
 
