1b6192e9fabb2e704573c9526fbead4bc9613efd
Nikita Goswami <113491032+Nikieta@users.noreply.github.com>
2023-10-21 11:10:24 +0000

    Modified

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,19 +1,25 @@
 const score=400;
 console.log(score);//400
 const balance=new Number(100);//100
-// console.log(balance)
+console.log(balance)
 
 // console.log(balance)//[Number:100] basically explicitly defining that balance is of Number type.
-// console.log(typeof(balance.toString()));//there are less properties for Numbers as compared to strings.
-// //here the numbers are converted to string and now the string functions are applicable on this number
-// console.log(typeof(balance))
-// //Property applicable on number,upto 2 decimal places.
+//key-value pair thus an object.
+console.log(balance);//here the balance is an object of type Number
+
+console.log(typeof(balance.toString()));
+//returns string
+//there are less properties for Numbers as compared to strings.
+// here the numbers are converted to string and now the string functions are applicable on this number
+console.log(typeof(balance)) //object
+// Property applicable on number,upto 2 decimal places.
 // console.log(balance.toFixed(2))
 // console.log(balance.toString().length);
 
 //INTERVIEW QUESTION PRECISION
 const otherNumber=1123.8966 //SIG FIGS
-// console.log(otherNumber.toPrecision(3))//returns a string,accepts 1 to 21 sig figs
+console.log(otherNumber.toPrecision(3))  //1.12e+3
+//returns a string,accepts 1 to 21 sig figs
 
 
 const hundreds=1000000;
@@ -21,21 +27,23 @@ const hundreds=1000000;
 // console.log(hundreds.toLocaleString('en-IN')) //10,00,000 //conversion to Indian Standard
 //Many functions can be performed on the Numbers like
 // Number.MIN_VALUE
-// Number.MAX_VALUE
 // Number.MIN_SAFE_INTEGER
-// Number.MAX_SAFE_INTEGER
+// Number.MAX_SAFE_INTEGER //Maximum integral value which is exact and there is no loss of precision involved.
+// Number.MAX_VALUE//Includes integers as well as floating point numbers.Maximum possible integer that can be stored in JavaScript.
+
+
 
 //+++++++++++++++++++ Maths +++++++++++++++++++++
 
-console.log(Math)//Object [Math] {}
+console.log(Math)// It is a Math library.Object [Math] {} which contains a lot of properties.
 
 //Contains properties and functions
-// console.log(Math.abs(4))//mod function.
-// console.log(Math.round(4.5));//5
+console.log(Math.abs(4))//mod function.
+console.log(Math.round(4.5));//5
 
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
-// console.log(Math.min(4,3,6,8));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,3,6,8));
 // console.log(Math.max(4,3,6,8)); calculating the minimum and maximum value in an array.
 console.log(Math.random())//will always lie between 0 and 1 both inclusive.
 console.log(Math.floor((Math.random()*10)+1))//for min value 1.
